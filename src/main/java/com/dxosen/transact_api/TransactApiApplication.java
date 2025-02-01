5b40fcaf979a8ec87db2e66cbe04b9ba6f7fc369
Gabriel Dario <dxosen@gmail.com>
2025-01-31 23:26:27 -0300

    initial commit

---
+++ src/main/java/com/dxosen/transact_api/TransactApiApplication.java
@@ -0,0 +1,13 @@
+package com.dxosen.transact_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxosen/transact_api/TransactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxosen.transact_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
